a7f1e0017a93f43f4ae42963bd8bb7f5526b2d42
Vlada <vadusha2006@yandex.ru>
2024-10-16 15:37:27 +0300

    DZ 241016

--- 241015/after_refactoring_vshabelnikova.cpp
+++ 241015/after_refactoring_vshabelnikova.cpp
@@ -12,16 +12,14 @@ int main(){
 		s = (1 + sin(2 * x)) / (1 - cos2x);
 		std::cout << "F(x1)= " << s << std::endl;
 	}
-	else
-		std::cout << "Неверный ввод для F(x1) " << std::endl;
+	else std::cout << "Неверный ввод для F(x1) " << std::endl;
 
 	double tanx = tan(x)
 	if  (tanx != 1){
 		s2 = (1 + tanx * tanx) / (1 - x * tanx);
 		std::cout << "F(X2)= " << s2 << std::endl;
 	}
-	else 
-		std::cout << "Неверный ввод для F(x2) " << std::endl;
+	else std::cout << "Неверный ввод для F(x2) " << std::endl;
 
 	system ("pause");
 	return 0;
