7c941879e4436e25b6a6d392951db63acd788706
Vlada <vadusha2006@yandex.ru>
2024-10-17 14:08:14 +0300

    DZ 241016

--- 241017/after_refactoring_vshabelnikova.cpp
+++ 241017/after_refactoring_vshabelnikova.cpp
@@ -6,23 +6,23 @@ int main(){
 	std::cout << "Введите угол" << std::endl;
 	std::cin >> angle;
 
-	double result_f1, cos2x = cos(2 * angle);
+	double result_f1;
 	if  (cos2x != 1){
 		result_f1 = 
 		(1 + sin(2 * angle)) 
 		/ 
-		(1 - cos2x);
+		(1 - cos(2 * angle));
 		std::cout << "F(x1)= " << result_f1 << std::endl;
 	}
 	else std::cout << "Неверный ввод для F(x1) " << std::endl;
 
-	double result_f2, tanx = tan(x)
+	double result_f2, tanx = tan(x);
 	if  (tanx != 1){
 		result_f2 = 
 		(1 + tanx * tanx) 
 		/ 
 		(1 - x * tanx);
-		std::cout << "F(X2)= " << result_f2 << std::endl;
+		std::cout << "F(x2)= " << result_f2 << std::endl;
 	}
 	else std::cout << "Неверный ввод для F(x2) " << std::endl;
 
